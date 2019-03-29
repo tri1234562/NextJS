744c86f8a5c3f5177a4c810c41131bb8c6370a1b
TRI HACK <minhtripr1605@gmail.com>
2019-03-29 16:50:49 +0700

    lan 2

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,32 @@
 
 
-import Document, {Main, Head, NextScript} from 'next/document';
+import Document, { Main, Head, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <html lang="en" >
+                <Head>
+                    <meta name="desciption" content="Just a test, every will be fine" />
+                    <meta name="viewport" content="width=device-width" />
+                    <meta name="robots" content="noindex, nofollow" />
+                    <meta charSet="utf-8" />
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" />
+                    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+                    <link href="https://fonts.googleapis.com/css?family=Germania+One" rel="stylesheet"></link>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+                <style global jsx>{`
+                body{
+                    font-family: 'Roboto', sans-serif;
+                }
+            `}</style>
+            </html>
+        )
+    }
+}
+
+export default MyDocument;
